fe00c996ebb5527b40215cf87cd0722b242d2a3d
Goncalo Silva (CTW) <Goncalo.Silva@ctw.bmwgroup.com>
2021-05-05 18:25:28 +0100

    import

---
+++ src/main/java/pt/goncalo/springboot/mqttintegration/configuration/JustPrintMessageReceiverConfiguration.java
@@ -0,0 +1,48 @@
+package pt.goncalo.springboot.mqttintegration.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class JustPrintMessageReceiverConfiguration {
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter("tcp://YOUR_BROKER_HERE:1883", "testClient",
+                        "topic1", "topic2");
+        adapter.setCompletionTimeout(5000);
+        //adapter.setRecoveryInterval();
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println("message is " + message.getPayload());
+            }
+
+        };
+    }
+}
---
+++ src/test/java/pt/goncalo/springboot/mqttintegration/MqttIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.goncalo.springboot.mqttintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
